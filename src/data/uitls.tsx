ec80f5cff589eb443d947a6ec893b45103124582
serhii <sergiydotsenko@gmail.com>
2022-05-30 18:53:46 +0100

    improved income per nft calc

--- src/data/uitls.tsx
+++ src/data/uitls.tsx
@@ -4,14 +4,17 @@ import Nft from "../types/Nft";
 
 export const MAX_BP = 10000;
 
-export function calcIncomePerNft(item: Nft, ctx: AppContextType): number {
+export function calcBasicIncomePerNft(ctx: AppContextType): number {
     if (ctx.platform.emissionType == 3) { // fixed per nft, all time
-
-        const multBb = ctx.nftMultMap[item.address.toBase58()];
-        return ctx.platform.basicDailyIncome * multBb / MAX_BP;
-
+        return ctx.platform.basicDailyIncome;
     } else {
         toast.error(`Unable to calc income per nft for emission type of platform (${ctx.platform.emissionType})`)
         return 0;
     }
+}
+
+export function calcIncomePerNft(item: Nft, ctx: AppContextType): number {
+    const basicIncomePerNft = calcBasicIncomePerNft(ctx);
+    const multBb = ctx.nftMultMap[item.address.toBase58()];
+    return basicIncomePerNft * multBb / MAX_BP;
 }
