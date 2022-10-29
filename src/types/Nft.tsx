c4f7c6e0fa59dfd1a83e510d5f9b3a01023e59aa
serhii <sergiydotsenko@gmail.com>
2022-10-29 15:07:30 +0100

    add flags

--- src/types/Nft.tsx
+++ src/types/Nft.tsx
@@ -1,11 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
-import { StakingReceipt } from "../blockchain/idl/accounts/StakingReceipt";
-import { StakingContextType } from "../state/stacking";
-import { Config } from "./config";
 
 export default interface Nft {
     address: PublicKey
     name: string
     image: string
     props?: { [key: string]: any }
+    flags?: number
 }
